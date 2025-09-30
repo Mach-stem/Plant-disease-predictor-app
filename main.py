0873404dfe08129e246004f6c0c378327301b313
AbhijithJ1 <abhijithjayanthan7@gmail.com>
2025-09-30 17:29:09 +0530

    fix: training shuffle issue

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ for image_batch, labels_batch in dataset.take(1):
         test_ds = test_ds.skip(6)
         len(test_ds)
 
-def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=1000):
     assert (train_split + test_split + val_split) == 1
 
     ds_size = len(ds)
@@ -62,7 +62,7 @@ train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
 len(train_ds)
 len(val_ds)
 len(test_ds)
-train_ds = train_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
+train_ds = train_ds.shuffle(1000).cache().batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 test_ds = test_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 resize_and_rescale = tf.keras.Sequential([
